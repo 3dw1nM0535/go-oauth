d59bd2060aa7f54ec81ce42bf0fab8ea8d83e232
3dw1nM0535 <workockmoses@gmail.com>
2019-12-14 17:08:30 +0300

    ft(google oauth)

--- go.mod
+++ go.mod
@@ -3,14 +3,18 @@ module github.com/3dw1nM0535/go-auth
 go 1.13
 
 require (
-	github.com/gin-gonic/gin v1.5.0 // indirect
+	github.com/gin-contrib/sessions v0.0.1
+	github.com/gin-gonic/contrib v0.0.0-20191209060500-d6e26eeaa607
+	github.com/gin-gonic/gin v1.5.0
 	github.com/go-playground/universal-translator v0.17.0 // indirect
+	github.com/gorilla/sessions v1.2.0 // indirect
 	github.com/json-iterator/go v1.1.8 // indirect
 	github.com/leodido/go-urn v1.2.0 // indirect
 	github.com/mattn/go-isatty v0.0.11 // indirect
-	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
-	github.com/modern-go/reflect2 v1.0.1 // indirect
+	golang.org/x/net v0.0.0-20191209160850-c0dbc17a3553 // indirect
+	golang.org/x/oauth2 v0.0.0-20191202225959-858c2ad4c8b6
 	golang.org/x/sys v0.0.0-20191210023423-ac6580df4449 // indirect
+	google.golang.org/appengine v1.6.5 // indirect
 	gopkg.in/go-playground/validator.v9 v9.30.2 // indirect
 	gopkg.in/yaml.v2 v2.2.7 // indirect
 )
---
+++ main.go
@@ -0,0 +1,132 @@
+package main
+
+import (
+	"crypto/rand"
+	"encoding/base64"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/gin-gonic/contrib/sessions"
+	"github.com/gin-gonic/gin"
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/google"
+)
+
+// Credentials : user data struct/composite literals
+type Credentials struct {
+	Cid     string `json:"client_id"`
+	Csecret string `json:"client_secret"`
+}
+
+// User : retrieved and authenticated
+type User struct {
+	Sub           string `json:"sub"`
+	Name          string `json:"name"`
+	GivenName     string `json:"given_name"`
+	FamilyName    string `json:"family_name"`
+	Profile       string `json:"profile"`
+	Picture       string `json:"picture"`
+	Email         string `json:"email"`
+	EmailVerified string `json:"email_verified"`
+	Gender        string `json:"gender"`
+}
+
+var cred Credentials
+var conf *oauth2.Config
+var state string
+var store = sessions.NewCookieStore([]byte("secret"))
+
+// indexHandler : handle index page
+func indexHandler(c *gin.Context) {
+	c.HTML(http.StatusOK, "index.tmpl", gin.H{})
+}
+
+// getLogin : get state from authentication url
+func getLoginURL(state string) string {
+	return conf.AuthCodeURL(state)
+}
+
+// randToken : generate random token
+func randToken() string {
+	b := make([]byte, 32)
+	rand.Read(b)
+	return base64.StdEncoding.EncodeToString(b)
+}
+
+// LoginHandler : store token in session
+func loginHandler(c *gin.Context) {
+	state = randToken()
+	session := sessions.Default(c)
+	session.Set("state", state)
+	session.Save()
+	c.Writer.Write([]byte("<html><title>Golang Google</title> <body><a href='" + getLoginURL(state) + "'><button>Login with Google</button></a></body></html>"))
+}
+
+func init() {
+	var c Credentials
+	file, err := ioutil.ReadFile("./cred.json")
+	log.Println(file)
+	if err != nil {
+		fmt.Printf("File error: %v\n", err)
+		os.Exit(1)
+	}
+	log.Println(file)
+	json.Unmarshal(file, &c)
+
+	conf = &oauth2.Config{
+		ClientID:     c.Cid,
+		ClientSecret: c.Csecret,
+		RedirectURL:  "http://localhost:9090/auth",
+		Scopes: []string{
+			"https://www.googleapis.com/auth/userinfo.email",
+		},
+		Endpoint: google.Endpoint,
+	}
+}
+
+func authHandler(c *gin.Context) {
+	// Check state validity
+	session := sessions.Default(c)
+	retrievedState := session.Get("state")
+	if retrievedState != c.Request.URL.Query().Get("state") {
+		c.AbortWithError(http.StatusUnauthorized, fmt.Errorf("Invalid session state: %s", retrievedState))
+		return
+	}
+
+	// Handle the exchange code to initiate transport
+	token, err := conf.Exchange(oauth2.NoContext, c.Query("code"))
+	if err != nil {
+		c.AbortWithError(http.StatusBadRequest, err)
+		log.Println("Error during credential exchange: ", err.Error())
+		return
+	}
+	// Construct the client
+	client := conf.Client(oauth2.NoContext, token)
+	res, err := client.Get("https://www.googleapis.com/oauth2/v3/userinfo")
+	if err != nil {
+		c.AbortWithError(http.StatusBadRequest, err)
+		log.Println("Error communicating with google API: ", err.Error())
+		return
+	}
+
+	defer res.Body.Close()
+	data, _ := ioutil.ReadAll(res.Body)
+	log.Println("Response Body: ", string(data))
+}
+
+func main() {
+	app := gin.New()
+	app.Use(sessions.Sessions("goquestsession", store))
+	app.Static("/css", "./static/css")
+	app.Static("/img", "./static/img")
+	app.LoadHTMLGlob("templates/*")
+
+	app.GET("/", indexHandler)
+	app.GET("/login", loginHandler)
+	app.GET("/auth", authHandler)
+	app.Run("127.0.0.1:9090")
+}
