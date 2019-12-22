3c2fc70643f8ea2c9a435a35927d1c6e7357e070
3dw1nM0535 <workockmoses@gmail.com>
2019-12-22 17:23:13 +0300

    ft(add utility func)

--- utils/env.go
+++ utils/env.go
@@ -3,13 +3,27 @@ package utils
 import (
 	"log"
 	"os"
+	"strconv"
 )
 
 // MustGet : returns Environment variable otherwise error
 func MustGet(envVar string) string {
 	v := os.Getenv(envVar)
 	if v == "" {
-		log.Panicf("Env variable missing: " + v)
+		log.Panicf("Env variable missing: " + envVar)
 	}
 	return v
+}
+
+// MustGetBool : returns bool Environment variable otherwise error
+func MustGetBool(envVar string) bool {
+	v := os.Getenv(envVar)
+	if v == "" {
+		log.Panicf("Env variable missing: " + envVar)
+	}
+	b, err := strconv.ParseBool(v)
+	if err != nil {
+		log.Panicf("Error parsing env variable: " + err.Error())
+	}
+	return b
 }
